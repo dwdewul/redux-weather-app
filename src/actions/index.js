e48f577162a51ef4b9f34a952a571db7c96fbe8c
David DeWulf <dwdewul@gmail.com>
2017-08-24 09:54:19 -0500

    Finalize project with all functionality Add favicon Abstract out Chart component Add average line and a numeric value for temps, etc. to chart component Implement Google Map instead of listing city name Add CSS styling to make components render in uniform

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '75b28b464293857bd13888c8254ee39c';
-let weatherURL = `api.openweathermap.org/data/2.5/forecast?q=
-                ${city_name},us&appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const URL = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(URL);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request,
+    }
+}
